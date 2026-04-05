7662f40921431605b9e123c6ae1a7fa89535330c
zaselalk <43272744+zaselalk@users.noreply.github.com>
2026-04-05 21:49:27 +0530

    :heavy_plus_sign: Add analytics configuration to Angular and update Spring Boot dependencies

--- api/src/main/java/com/zasela/api/ApiApplication.java
+++ api/src/main/java/com/zasela/api/ApiApplication.java
@@ -3,13 +3,9 @@ package com.zasela.api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
 @SpringBootApplication
 public class ApiApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(ApiApplication.class, args);
     }
-
-
 }
