c2bc10d426ae9fa0764328a243aa149b72756346
zaselalk <43272744+zaselalk@users.noreply.github.com>
2026-04-05 21:34:07 +0530

    :heavy_plus_sign: Initialize Spring Boot API project with Gradle setup and basic structure

---
+++ api/src/test/java/com/zasela/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zasela.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
